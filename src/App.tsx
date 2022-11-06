c55b254c2f48276961ca5b5b6acb06a7956cd6f4
Alex Sin <sinalexandru@gmail.com>
2022-11-06 22:00:09 +0200

    add mobile support

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import Weather from "./components/Weather/Weather";
 
 function App() {
+	const isMobile = window.innerWidth <= 600;
+
 	return (
 		<div className="App">
-			<Weather showLocation={true} size="large" />
+			<Weather showLocation={true} size={isMobile ? "small" : "large"} />
 		</div>
 	);
 }
